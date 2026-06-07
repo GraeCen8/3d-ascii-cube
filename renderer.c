6a76d0ed4e2a353e4bdf7855795efaf32bb103e5
grae ceney <gceney7@gmail.com>
2026-06-07 13:10:31 +0100

    added some buffer funcs

--- renderer.c
+++ renderer.c
@@ -14,3 +14,20 @@ void enableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+#define WIDTH 80
+#define HEIGHT 25
+
+char screen[HEIGHT][WIDTH];
+
+void clearBuffer(void) {
+  for (int y=0; y < HEIGHT; y++)
+    for (int x=0; x < WIDTH; x++)
+      screen[y][x] = ' ';
+}
+
+void drawPoint(int x, int y, char c) {
+  if (x < 0 || x >= WIDTH) return;
+  if (y < 0 || y >= HEIGHT) return;
+
+  screen[y][x] = c; 
+}
